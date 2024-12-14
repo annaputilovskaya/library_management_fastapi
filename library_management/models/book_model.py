2a4e6dffb2a8c6db5f030409cedc29b8f1239c2a
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-14 12:10:28 +0300

    create model Book

---
+++ library_management/models/book_model.py
@@ -0,0 +1,25 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.base import Base
+from models.mixins.int_id_pk import IntIdPkMixin
+
+
+class Book(Base, IntIdPkMixin):
+    """
+    Модель книги.
+    """
+
+    title: Mapped[str]
+    description: Mapped[str]
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id", ondelete="CASCADE"))
+    available_amount: Mapped[int]
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"({self.id=}, {self.title=}, {self.description=}, {self.author_id=}, {self.available_amount=})"
+        )
