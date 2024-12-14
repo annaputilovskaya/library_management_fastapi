e0dbd35520e31d80b45e342584e4bd2ea0da8c1c
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-14 12:07:18 +0300

    create model Author

---
+++ library_management/models/author_model.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from sqlalchemy.orm import Mapped
+
+from core.base import Base
+from models.mixins.int_id_pk import IntIdPkMixin
+
+
+class Author(Base, IntIdPkMixin):
+    """
+    Модель автора.
+    """
+
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    born_at: Mapped[date]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.id=}, {self.first_name=}, {self.last_name=}, {self.born_at=})"
