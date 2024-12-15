3cba65e32830637ee8e36425ec2ef959629cf1cf
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-15 19:12:03 +0300

    change type of available amount to PositiveInt

--- library_management/schemas/book_schemas.py
+++ library_management/schemas/book_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PositiveInt
 
 
 class BookSchema(BaseModel):
@@ -9,7 +9,7 @@ class BookSchema(BaseModel):
     title: str = Field(title="Название", max_length=100)
     description: str | None = None
     author_id: int = Field(title="Автор")
-    available_amount: int | None = None
+    available_amount: PositiveInt | None = None
 
 
 class BookReadSchema(BookSchema):
