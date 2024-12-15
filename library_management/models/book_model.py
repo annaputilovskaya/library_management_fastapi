e2c27b9f7a54c688066dfdafffc17270165b2163
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-15 12:04:50 +0300

    add default to book available amount

--- library_management/models/book_model.py
+++ library_management/models/book_model.py
@@ -13,7 +13,7 @@ class Book(Base, IntIdPkMixin):
     title: Mapped[str]
     description: Mapped[str | None]
     author_id: Mapped[int] = mapped_column(ForeignKey("author.id", ondelete="CASCADE"))
-    available_amount: Mapped[int]
+    available_amount: Mapped[int] = mapped_column(default=1)
 
     author: Mapped["Author"] = relationship(back_populates="books")
 
