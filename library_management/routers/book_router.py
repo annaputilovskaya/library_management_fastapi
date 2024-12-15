e4ed0fe602acff0bf5cbaf5b72e4c562d07dbdb8
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-15 12:22:04 +0300

    add get_book

--- library_management/routers/book_router.py
+++ library_management/routers/book_router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import db_helper
-from crud.book import book_create
+from crud.book import book_create, get_book_list
+from crud.dependencies import book_by_id
+from models.book_model import Book
 from schemas.book_schemas import BookReadSchema, BookSchema
 
 router = APIRouter(
@@ -22,3 +24,19 @@ async def add_book(
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     return await book_create(book=book, session=session)
+
+
+@router.get("", summary="Список книг", response_model=list[BookReadSchema])
+async def get_all_books(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await get_book_list(session=session)
+
+
+@router.get(
+    "/{book_id}",
+    summary="Информация о книге по id",
+    response_model=BookReadSchema,
+)
+async def get_book(book: Book = Depends(book_by_id)):
+    return book
