87c6512645dda369105ada278c1669aac2c237f1
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-15 20:45:36 +0300

    include borrow_router

--- library_management/routers/__init__.py
+++ library_management/routers/__init__.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter
 
 from routers.author_router import router as author_router
 from routers.book_router import router as book_router
+from routers.borrow_router import router as borrow_router
+
 
 router = APIRouter()
 router.include_router(author_router)
 router.include_router(book_router)
+router.include_router(borrow_router)
